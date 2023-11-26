efa7bb766b30cb897b6f96d90bebae143856168d
Ashish Dhakal <140738070+ashishdkl@users.noreply.github.com>
2023-11-26 20:22:25 +0545

    Add files via upload

---
+++ adding string and integer.vb
@@ -0,0 +1,17 @@
+﻿Module Module1
+
+    Sub Main()
+        'adding string and integer
+        Dim i As Integer = 10
+        Dim b As String = "ashish"
+        Dim result As String
+        result = i & b ' we can also write result=i+b if there is same data type s
+        'dim i as string="ashish"
+        'dim b as string="dhakal"
+        Console.WriteLine(result)
+        Console.ReadLine()
+
+
+    End Sub
+
+End Module
