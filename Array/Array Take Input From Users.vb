c6cb46e05eab5e7ac84d31b9ccc7834c77843d60
Ashish Dhakal <140738070+ashishdkl@users.noreply.github.com>
2023-12-02 17:01:11 +0545

    Add files via upload

---
+++ Array/Array Take Input From Users.vb
@@ -0,0 +1,19 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim array(7) As Integer
+        Console.WriteLine("Enter your values:")
+        For i As Integer = 0 To 7
+            array(i) = Console.ReadLine
+
+        Next
+        For j As Integer = 0 To 7
+            Console.WriteLine(array(j))
+
+
+        Next
+        Console.ReadLine()
+
+    End Sub
+
+End Module
