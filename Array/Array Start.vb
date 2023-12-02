16ca2daf5dadb4fcc7962277a13573b03044bbba
Ashish Dhakal <140738070+ashishdkl@users.noreply.github.com>
2023-12-02 16:37:14 +0545

    Add files via upload

---
+++ Array/Array Start.vb
@@ -0,0 +1,20 @@
+﻿Module Module1
+    Sub Main()
+        Dim num(5) As Integer
+        Dim x(0 To 5) As Integer
+
+        num(0) = 10
+        num(1) = 450
+        num(2) = 430
+        num(3) = 430
+        num(4) = 40
+        num(5) = 50
+
+        For i As Integer = 0 To 5
+            Console.WriteLine(num(i))
+        Next
+
+        Console.ReadLine()
+    End Sub
+
+End Module
